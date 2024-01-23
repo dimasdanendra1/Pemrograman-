63e34afc2b1f800377fa72406d356572efc60d27
Dimas danendra <148471058+dimasdanendra1@users.noreply.github.com>
2024-01-23 10:38:50 +0700

    Dimas danendra
    220511016

---
+++ Pemograman 2/Pertemuan 8 UTS/segitiga.py
@@ -0,0 +1,13 @@
+class Segitiga:
+    def _init_(self, alas, tinggi):
+        self.alas = alas
+        self.tinggi = tinggi
+
+    def get_luas(self):
+        return 0.5 * self.alas * self.tinggi
+
+segitiga1 = Segitiga(5, 10)
+segitiga2 = Segitiga(10, 10)
+
+print('Luas segitiga1:', segitiga1.get_luas())
+print('Luas segitiga2:', segitiga2.get_luas())
